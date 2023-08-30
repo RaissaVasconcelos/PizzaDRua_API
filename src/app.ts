241806885fa02d6311debe477fd1ae06afbe9f47
Juno <ganimedes409@gmail.com>
2023-08-30 12:05:21 -0300

    feat: Add in file app setErrorHandler

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+import { ZodError } from 'zod'
+import {fromZodError} from 'zod-validation-error'
+import { env } from "./env";
+
+export const app = fastify();
+
+app.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) {
+       return  reply.status(400).send({message: 'Validation error', issues: fromZodError(error)})
+    }
+    if (env.NODE_ENV !== 'production') {
+        console.error(error)
+    }
+    return reply.status(500).send({ message: 'Internal server error.' })
+})
