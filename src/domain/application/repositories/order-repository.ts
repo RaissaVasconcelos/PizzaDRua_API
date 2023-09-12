e410e4af2417586d1079baeeed91ec184eefeccb
Raissa <rai.celos20@gmail.com>
2023-09-12 18:39:56 -0300

    feat: create repositoryOder

--- src/domain/application/repositories/order-repository.ts
+++ src/domain/application/repositories/order-repository.ts
@@ -3,4 +3,6 @@ import { Order } from '../../enterprise/entities/order'
 export interface orderRepository {
   create(order: Order): Promise<void>
   findById(id: string): Promise<Order | null>
+  findMany(): Promise<Order[] | []>
+  update(id: string, order: Order): Promise<Order | null>
 }
