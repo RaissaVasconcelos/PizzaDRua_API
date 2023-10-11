2719c451bbd3153bad90b39ec94b24a8f3bb90c3
Raissa <rai.celos20@gmail.com>
2023-10-11 19:45:04 -0300

    feat: create function getTaxCustomer

--- src/domain/application/repositories/address-repository.ts
+++ src/domain/application/repositories/address-repository.ts
@@ -3,6 +3,7 @@ import { Address } from "../../enterprise/entities/address"
 export interface AddressRepository {
     create(address: Address): Promise<Address>
     findById(id: string): Promise<Address | null>
+    find(client: string): Promise<Address[]>
     findMany(customerId: string): Promise<Address[]>
     update(address: Address): Promise<void>
     delete(id: string): Promise<void>
